04f70706cc183cd186c941107f436e183bf4aa57
vibhuk10 <vibhuk10@gmail.com>
2020-12-29 01:10:40 -0800

    scrape updates

--- PFRScraping.R
+++ PFRScraping.R
@@ -5,7 +5,6 @@ library(rvest)
 scrapeData = function(urlprefix, urlend, startyr, endyr) {
   master = data.frame()
   for (i in startyr:endyr) {
-    cat('Loading Year', i, '\n')
     URL = paste(urlprefix, as.character(i), urlend, sep = "")
     table = html_table(html_nodes(read_html(URL),"table")[[1]])
     table$Year = i
@@ -18,23 +17,42 @@ scrapeData = function(urlprefix, urlend, startyr, endyr) {
 scrapePFRData = function(urlend, startyr, endyr) {
   df = data.frame()
   for (i in startyr:endyr) {
-    cat('Loading Year', i, '\n')
     URL = paste('https://www.pro-football-reference.com/years/', as.character(i), urlend, sep = "")
     table = html_table(html_nodes(read_html(URL),"table")[[1]])
+    #clean data
+    for(a in ncol(table)) {
+      if(colnames(table)[a] ==  "") {
+        colnames(table) <- as.character(table[1,])
+        table <- table[-c(1),]
+      }
+    }
     table$Year = i
     df = rbind(table, df)
   }
   df
 }
 
-# clean up data
-cleanPFRData = function(df) {
-  colnames(df) <- as.character(df[1,])
-  df <- df[-c(1),]
-  df
+
+# get variable from df
+getVar <- function(df, row, column) {
+  val <- df[row,column]
+  val <- val[[1]]
+  val
 }
 
-standard_passing_20 <- scrapePFRData('/passing.htm', 2020, 2020)
-standard_rushing_20 <- scrapePFRData('/rushing.htm', 2020, 2020) %>% cleanPFRData()
+PFRDataNames <- 
+  tibble(
+    varName = c('standard_passing_20','standard_rushing_20'),
+    urlend = c('/passing.htm', '/rushing.htm'),
+    startyr = c(2020, 2020),
+    endyr = c(2020, 2020),
+    clean = c(0, 1)
+  )
 
+for(i in 1:nrow(PFRDataNames)) {
+  assign(getVar(PFRDataNames, i, 1), scrapePFRData(getVar(PFRDataNames, i, 2), getVar(PFRDataNames, i, 3), getVar(PFRDataNames, i, 4)))
+}
+
+#standard_passing_20 <- scrapePFRData('/passing.htm', 2020, 2020)
+#standard_rushing_20 <- scrapePFRData('/rushing.htm', 2020, 2020)
 #standard_passing_20 <- scrapeData('https://www.pro-football-reference.com/years/', '/passing.htm', 2020, 2020)
