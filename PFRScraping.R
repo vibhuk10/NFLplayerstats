bd4472285e8e255acc143b97014ad9ac02c914ea
vibhuk10 <vibhuk10@gmail.com>
2020-12-29 20:12:42 -0800

    updates to PFR scraping

--- PFRScraping.R
+++ PFRScraping.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 library(rvest)
 
+# get variable from df
+getVar <- function(df, row, column) {
+  val <- df[row,column]
+  val <- val[[1]]
+  val
+}
+
 # general scarping
 scrapeData = function(urlprefix, urlend, startyr, endyr) {
   master = data.frame()
@@ -13,46 +20,83 @@ scrapeData = function(urlprefix, urlend, startyr, endyr) {
   return(master)
 }
 
-# PFR scarping
-scrapePFRData = function(urlend, startyr, endyr) {
+# PFR scraping
+scrapePFRData = function(urlend, startyr, endyr, numTable) {
   df = data.frame()
   for (i in startyr:endyr) {
     URL = paste('https://www.pro-football-reference.com/years/', as.character(i), urlend, sep = "")
-    table = html_table(html_nodes(read_html(URL),"table")[[1]])
-    #clean data
-    for(a in ncol(table)) {
+    table = html_table(html_nodes(read_html(URL),"table")[[numTable]])
+    #correct column names
+    for(a in 1:ncol(table)) {
       if(colnames(table)[a] ==  "") {
         colnames(table) <- as.character(table[1,])
         table <- table[-c(1),]
       }
     }
+    #remove extra column names
+    extraRowNums <- c()
+    for(b in 1:nrow(table)) {
+      if(colnames(table)[2] == getVar(table,b,2)) {
+        extraRowNums <- append(extraRowNums, b)
+      }
+    }
+    table <- table[-c(extraRowNums),]
+    #remove asterisk in the player name
+    for(c in 1:ncol(table)) {
+      if(colnames(table)[c] ==  "Player") {
+        for(d in 1:nrow(table)) {
+          if(grepl("*", getVar(table, d, c), fixed = TRUE)) {
+            table[d,c] = gsub("*","", getVar(table, d, c), fixed = TRUE)
+          }
+          table[d,c] = trimws(getVar(table, d, c))
+        }
+      }
+    }
+    # reset the row numbers
+    rownames(table) <- seq(length=nrow(table))
     table$Year = i
-    df = rbind(table, df)
+    df <- rbind(table, df)
   }
   df
 }
 
-
-# get variable from df
-getVar <- function(df, row, column) {
-  val <- df[row,column]
-  val <- val[[1]]
-  val
-}
-
+# tibble for each data set that is scraped through scrapePFRData
 PFRDataNames <- 
   tibble(
-    varName = c('standard_passing_20','standard_rushing_20'),
-    urlend = c('/passing.htm', '/rushing.htm'),
-    startyr = c(2020, 2020),
-    endyr = c(2020, 2020),
-    clean = c(0, 1)
+    varName = c('stanPass_20','stanRush_20','stanRecv_20', 'advPassAY_20', 'advPassAcc_20'),
+    urlend = c('/passing.htm', '/rushing.htm', '/receiving.htm', '/passing_advanced.htm', '/passing_advanced.htm'),
+    startyr = c(2020, 2020, 2020, 2020, 2020),
+    endyr = c(2020, 2020, 2020, 2020, 2020),
+    numTable = c(1, 1, 1, 1, 2)
   )
 
+# for loop to scrape each dataset in PFRDataNames through ScrapePFRData
 for(i in 1:nrow(PFRDataNames)) {
-  assign(getVar(PFRDataNames, i, 1), scrapePFRData(getVar(PFRDataNames, i, 2), getVar(PFRDataNames, i, 3), getVar(PFRDataNames, i, 4)))
+  assign(getVar(PFRDataNames, i, 1), scrapePFRData(getVar(PFRDataNames, i, 2), getVar(PFRDataNames, i, 3), getVar(PFRDataNames, i, 4), getVar(PFRDataNames, i, 5)))
 }
 
-#standard_passing_20 <- scrapePFRData('/passing.htm', 2020, 2020)
-#standard_rushing_20 <- scrapePFRData('/rushing.htm', 2020, 2020)
-#standard_passing_20 <- scrapeData('https://www.pro-football-reference.com/years/', '/passing.htm', 2020, 2020)
+# clean PFR data for downloaded files
+cleanPFRData = function(df, year) {
+  #correct column names
+  if(colnames(df)[1] ==  "X1") {
+    colnames(df) <- as.character(df[1,])
+    df <- df[-c(1),]
+  }
+  #remove asterisk in the player name
+  for(c in 1:ncol(df)) {
+    if(colnames(df)[c] ==  "Player") {
+      for(d in 1:nrow(df)) {
+        if(grepl("*", getVar(df, d, c), fixed = TRUE)) {
+          df[d,c] = gsub("*","", getVar(df, d, c), fixed = TRUE)
+        }
+        df[d,c] = trimws(getVar(df, d, c))
+      }
+    }
+  }
+  #add the year
+  df$Year = year
+  df
+}
+
+# advPassAcc_20 <- read_csv("data-raw/advPassAcc_2020.csv")
+# advPassAcc_20 <- cleanPFRData(advPassAcc_20, 2020)
